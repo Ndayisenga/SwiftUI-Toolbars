55ab982bd540d4af1b4764b21b3aab9ad8198d88
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-18 01:17:06 +0200

    Initial Commit

---
+++ SwiftUIToolbars/SwiftUIToolbarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToolbarsApp.swift
+//  SwiftUIToolbars
+//
+//  Created by Ndayisenga Jean Claude on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToolbarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
